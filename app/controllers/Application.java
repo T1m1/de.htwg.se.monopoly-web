147b3a03e5fb3d9e323a447f14981913996d4f1a
SteffenGorenflo <stgorenf@3DLAB18.htwg-konstanz.de>
2014-10-29 11:34:00 +0100

    add implementation and url mapping

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,17 +2,63 @@ package controllers;
 
 import de.htwg.monopoly.controller.IController;
 import de.htwg.monopoly.game.Monopoly;
+import de.htwg.monopoly.observer.Event;
+import de.htwg.monopoly.observer.IObserver;
+import de.htwg.monopoly.util.IMonopolyUtil;
+import de.htwg.monopoly.util.MonopolyUtils;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+public class Application extends Controller implements IObserver{
 
-public class Application extends Controller {
+	static IController controller;
 
-    static IController controller = Monopoly.getInstance().getController();
+	public static Result index() {
 
-    public static Result index() {
+		return ok(views.html.index.render("Hello Play Framework", controller));
+	}
 
-        return ok(views.html.index.render("Hello Play Framework", controller));
-    }
-    
+	public static Result startGame(Integer number) {
+		
+		controller = Monopoly.getInstance().getController();
+		
+		// start logger
+		Monopoly.getInstance().getTextUI().printInitialisation();
+
+		// check if a correct number of players is committed
+		if (!MonopolyUtils.verifyPlayerNumber(number)) {
+			return ok(views.html.index.render(
+					"Wrong number of players entered!", controller));
+		}
+
+		// fill it for now with predefined strings TODO: change implementation
+		String[] names = new String[number];
+		for (int i = 0; i < names.length; i++) {
+			names[i] = "Player " + i;
+		}
+
+		controller.startNewGame(number, names);
+
+		controller.startTurn();
+
+		return ok(views.html.index.render("Hello Play Framework " + number, controller));
+	}
+
+	public static Result endGame() {
+
+		controller.endTurn();
+		controller.exitGame();
+
+		return ok("END GAME");
+	}
+
+	public void update(Event arg0) {
+		// TODO Auto-generated method stub		
+	}
+
+	public void update(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
